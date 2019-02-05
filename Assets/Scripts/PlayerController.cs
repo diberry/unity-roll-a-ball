a13a3fffb5f386f57022493e8320aa6f01aa67f5
Dina Berry <diberry@microsoft.com>
2019-02-05 06:27:02 -0800

    starting with half way through video 5, creating collectibles

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    // called on first frame game is active
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //update - before rendering frame
+    void Update()
+    {
+
+
+    }
+    //fixed update -- before physics
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+
+    }
+}
